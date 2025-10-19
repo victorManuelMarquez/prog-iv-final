3538500e423e0de30fc4a20c1c4a666d5421498a
victor <victormmla3lm@gmail.com>
2025-10-19 09:03:25 -0300

    Demo: Se agregó un atajo de teclado para mostrar la ventana de configuración.

--- src/ar/com/baden/gui/MainFrame.java
+++ src/ar/com/baden/gui/MainFrame.java
@@ -2,15 +2,20 @@ package ar.com.baden.gui;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
 
 public class MainFrame extends JFrame {
 
     public MainFrame(String title) throws HeadlessException {
         super(title);
+        // variables
+        int ctrlAltModifier = KeyEvent.CTRL_DOWN_MASK | KeyEvent.ALT_DOWN_MASK;
+
         // componentes
         setJMenuBar(new JMenuBar());
         JMenu fileMenu = new JMenu("Archivo");
         JMenuItem settingsItem = new JMenuItem("Configuración");
+        settingsItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, ctrlAltModifier));
 
         // instalando componentes
         fileMenu.add(settingsItem);
