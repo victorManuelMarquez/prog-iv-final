b6df836edfa725afbb196cb2fd8e52bd76bf907b
victor <victormmla3lm@gmail.com>
2025-10-23 10:52:28 -0300

    Demo: Diseño preliminar para estilizar el tema "Metal".

--- src/ar/com/baden/gui/component/ThemesPanel.java
+++ src/ar/com/baden/gui/component/ThemesPanel.java
@@ -3,6 +3,10 @@ package ar.com.baden.gui.component;
 import ar.com.baden.main.App;
 
 import javax.swing.*;
+import javax.swing.plaf.metal.DefaultMetalTheme;
+import javax.swing.plaf.metal.MetalLookAndFeel;
+import javax.swing.plaf.metal.MetalTheme;
+import javax.swing.plaf.metal.OceanTheme;
 import java.awt.*;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
@@ -11,16 +15,25 @@ import java.beans.PropertyChangeEvent;
 public class ThemesPanel extends SettingsPanel implements ItemListener {
 
     private final JComboBox<UIManager.LookAndFeelInfo> lafCombo;
+    private final CardLayout cardLayout;
+    private final JPanel cardPanel;
+    private final String metal = "Metal";
 
     public ThemesPanel() {
+        super(null);
+
         // variables
+        cardLayout = new CardLayout();
+        GroupLayout groupLayout = new GroupLayout(this);
         LookAndFeel lookAndFeel = UIManager.getLookAndFeel();
         UIManager.LookAndFeelInfo[] installedLaf = UIManager.getInstalledLookAndFeels();
         DefaultComboBoxModel<UIManager.LookAndFeelInfo> lafModel = new DefaultComboBoxModel<>();
+        String currentLafName = null;
         for (UIManager.LookAndFeelInfo info : installedLaf) {
             lafModel.addElement(info);
             if (info.getClassName().equals(lookAndFeel.getClass().getName())) {
                 lafModel.setSelectedItem(info);
+                currentLafName = info.getName();
             }
         }
 
@@ -28,10 +41,29 @@ public class ThemesPanel extends SettingsPanel implements ItemListener {
         JLabel lafLabel = new JLabel("Tema");
         lafCombo = new JComboBox<>(lafModel);
         lafCombo.setRenderer(new LookAndFeelInfoRenderer());
+        cardPanel = new JPanel(cardLayout);
 
         // instalando componentes
-        add(lafLabel);
-        add(lafCombo);
+        cardPanel.add(new JLabel(""), "other");
+        cardPanel.add(new MetalPanel(), metal);
+        if (metal.equals(currentLafName)) {
+            cardLayout.show(cardPanel, metal);
+        }
+
+        groupLayout.setHorizontalGroup(groupLayout.createParallelGroup()
+                .addGroup(groupLayout.createSequentialGroup()
+                        .addComponent(lafLabel)
+                        .addComponent(lafCombo))
+                .addComponent(cardPanel));
+
+        groupLayout.setVerticalGroup(groupLayout.createSequentialGroup()
+                .addGroup(groupLayout.createParallelGroup()
+                        .addComponent(lafLabel)
+                        .addComponent(lafCombo))
+                .addComponent(cardPanel));
+
+        // ajustes
+        setLayout(groupLayout);
 
         // eventos
         lafCombo.addItemListener(this);
@@ -41,6 +73,7 @@ public class ThemesPanel extends SettingsPanel implements ItemListener {
     public void itemStateChanged(ItemEvent e) {
         if (e.getStateChange() == ItemEvent.SELECTED) {
             if (e.getItem() instanceof UIManager.LookAndFeelInfo info) {
+                cardLayout.show(cardPanel, metal.equals(info.getName()) ? metal : "other");
                 App.settings.setProperty("settings.lookAndFeel", info.getClassName());
             }
         }
@@ -69,6 +102,61 @@ public class ThemesPanel extends SettingsPanel implements ItemListener {
             if (value instanceof UIManager.LookAndFeelInfo info) {
                 JLabel label = (JLabel) component;
                 label.setText(info.getName());
+            }
+            return component;
+        }
+
+    }
+
+    static class MetalPanel extends JPanel {
+
+        public MetalPanel() {
+            super(new GridBagLayout());
+            // variables
+            GridBagConstraints gbc = new GridBagConstraints();
+            gbc.insets = new Insets(5, 5, 5, 5);
+            JComboBox<MetalTheme> metalCombo = createMetalThemesCombo();
+
+            // instalando componentes
+            add(new JLabel("Estilo"), gbc);
+            add(metalCombo, gbc);
+
+            // eventos
+            metalCombo.addItemListener(evt -> {
+                if (evt.getStateChange() == ItemEvent.SELECTED) {
+                    if (evt.getItem() instanceof MetalTheme theme) {
+                        App.settings.setProperty("settings.metalLookAndFeel", theme.getName());
+                    }
+                }
+            });
+        }
+
+        private JComboBox<MetalTheme> createMetalThemesCombo() {
+            MetalTheme currentTheme = MetalLookAndFeel.getCurrentTheme();
+            MetalTheme metalTheme;
+            metalTheme = currentTheme instanceof OceanTheme ? new DefaultMetalTheme() : new OceanTheme();
+
+            // componentes
+            JComboBox<MetalTheme> metalCombo = new JComboBox<>(new MetalTheme[] {
+                    currentTheme,
+                    metalTheme
+            });
+            metalCombo.setSelectedItem(currentTheme);
+            metalCombo.setRenderer(new MetalThemeListRenderer());
+            return metalCombo;
+        }
+
+    }
+
+    static class MetalThemeListRenderer extends DefaultListCellRenderer {
+
+        @Override
+        public Component getListCellRendererComponent(
+                JList<?> list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
+            Component component = super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
+            if (value instanceof MetalTheme theme) {
+                JLabel label = (JLabel) component;
+                label.setText(theme.getName());
             }
             return component;
         }
